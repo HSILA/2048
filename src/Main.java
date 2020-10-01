d6719a906a948f1f7cac2e46e881d15883c1b9fe
Ali <a.shiraee@gmail.com>
2020-10-01 19:54:20 +0330

    main class revision.

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,5 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         Graphic g = new Graphic();
-        //Game obj = new Game();
-//        obj.printer();
-//        for(int x=0 ; x<3 ; x++){
-//        obj.addNumber();
-//       }
-//        obj.printer();
-//        for(int x=0 ; x<10 ; x++){
-//            obj.addNumber();
-//            obj.printer();
-//            obj.Down();
-//            obj.printer();
-//        }
     }
 }
